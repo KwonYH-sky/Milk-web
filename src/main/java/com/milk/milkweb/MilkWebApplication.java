ecf435a8597b6bc180b1d496a0d7356075df37e0
KwonYH <yhoung11@gmail.com>
2023-08-12 16:31:22 +0900

    create repository

---
+++ src/main/java/com/milk/milkweb/MilkWebApplication.java
@@ -0,0 +1,13 @@
+package com.milk.milkweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilkWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milk/milkweb/MilkWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milk.milkweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
