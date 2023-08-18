8034155d9b9b0b4a088b2f40c74b3df52bd0ef56
KwonYH <yhoung11@gmail.com>
2023-08-18 17:05:13 +0900

    feat: Sping SecurityConfig 추가 - '/', '/member/**' 접근 모두 허용

--- src/main/java/com/milk/milkweb/config/SecurityConfig.java
+++ src/main/java/com/milk/milkweb/config/SecurityConfig.java
@@ -2,13 +2,29 @@ package com.milk.milkweb.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(authorize -> authorize
+				.requestMatchers(
+						new AntPathRequestMatcher("/"),
+						new AntPathRequestMatcher("/member/**")).permitAll()
+				.anyRequest().authenticated()
+		);
+
+		return http.build();
+	}
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
