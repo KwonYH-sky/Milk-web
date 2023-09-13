c91a6f0cd4c3a13eab02bb455ea37b184a68b096
KwonYH <yhoung11@gmail.com>
2023-09-14 00:30:00 +0900

    fix: static/css 인식 안되는 버그 수정

--- src/main/java/com/milk/milkweb/config/MvcConfig.java
+++ src/main/java/com/milk/milkweb/config/MvcConfig.java
@@ -3,6 +3,7 @@ package com.milk.milkweb.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
@@ -11,5 +12,11 @@ public class MvcConfig  extends WebMvcConfigurationSupport {
 	@Bean
 	public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
 		return new HiddenHttpMethodFilter();
+	}
+
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+		super.addResourceHandlers(registry);
 	}
 }
