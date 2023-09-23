8df5272f3efa6e49a7856523c6eb533380b75456
KwonYH <yhoung11@gmail.com>
2023-09-24 01:14:35 +0900

    fix: 게시물 좋아요 axios 비동기 버그 수정 - 직렬화/역직렬화 시 기본 생성자 필요함 - @Builder로 인해 기본 생성자 생성안됨 - @NoArgsConstructor로 기본 생성자 추가 - @AllArgsConstructor로 @Builder 유지 추후 확장 고려

--- src/main/java/com/milk/milkweb/dto/BoardLikeDto.java
+++ src/main/java/com/milk/milkweb/dto/BoardLikeDto.java
@@ -1,12 +1,17 @@
 package com.milk.milkweb.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-
+import lombok.NoArgsConstructor;
 
 @Getter @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BoardLikeDto {
 
 	private Long boardId;
+
+
 
 }
