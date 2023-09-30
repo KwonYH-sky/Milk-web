1447341c0303c77b13d05b4ef780c039cfc66e91
KwonYH <yhoung11@gmail.com>
2023-10-01 00:20:03 +0900

    fix: 댓글 작성 수정 - CommentListDto 생성자 추가 - 버튼 이벤트 추가

--- src/main/java/com/milk/milkweb/dto/CommentFormDto.java
+++ src/main/java/com/milk/milkweb/dto/CommentFormDto.java
@@ -1,9 +1,13 @@
 package com.milk.milkweb.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentFormDto {
 
 	private String comment;
