ed4dbc3f87185afcf98362c92c0751965d552480
KwonYH <yhoung11@gmail.com>
2023-11-05 00:39:49 +0900

    test: BoardRepositoryTest 코드 추가 - DailyBest & WeeklyBest 테스트 추가

--- src/test/java/com/milk/milkweb/repository/BoardRepositoryTest.java
+++ src/test/java/com/milk/milkweb/repository/BoardRepositoryTest.java
@@ -3,6 +3,7 @@ package com.milk.milkweb.repository;
 import com.milk.milkweb.config.TestQueryDslConfig;
 import com.milk.milkweb.constant.Role;
 import com.milk.milkweb.entity.Board;
+import com.milk.milkweb.entity.BoardLike;
 import com.milk.milkweb.entity.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,10 +16,12 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -32,6 +35,9 @@ class BoardRepositoryTest {
 
 	@Autowired
 	private MemberRepository memberRepository;
+
+	@Autowired
+	private BoardLikeRepository boardLikeRepository;
 
 	@BeforeEach
 	void setUp() {
@@ -80,6 +86,7 @@ class BoardRepositoryTest {
 	}
 
 	@Test
+	@DisplayName("메인 전체 게시판 불러오기")
 	void findMainBoards() {
 		// given
 		List<Board> boards = IntStream.rangeClosed(0, 10).mapToObj(e -> createTestBoard()).toList();
@@ -93,6 +100,69 @@ class BoardRepositoryTest {
 		Assertions.assertThat(mainBoards.size()).as("10개 인지").isEqualTo(10);
 	}
 
+	@Test
+	@DisplayName("데일리 베스트 찾기 테스트")
+	void findDailyBestBoards() {
+		// given
+		List<Board> boards = IntStream.rangeClosed(0, 7).mapToObj(e -> createTestBoard()).toList();
+		boardRepository.saveAll(boards);
+
+		boards.forEach(board -> IntStream.range(0, 3).forEach(i -> boardLikeRepository.save(createTestBoardLike(board))));
+
+		// when
+		List<Board> dailyBoards = boardRepository.findDailyBest();
+
+		// then
+		Assertions.assertThat(dailyBoards).as("비어있는지 여부").isNotEmpty();
+		Assertions.assertThat(dailyBoards.size()).as("5개인지").isEqualTo(5);
+		Assertions.assertThat(dailyBoards.stream().findFirst().get().getTitle()).isEqualTo("테스트");
+	}
+
+	@Test
+	@DisplayName("데일리 베스트 못 찾기 테스트 - 좋아요가 없을 시")
+	void notFindDailyBestBoards() {
+		// given
+		List<Board> boards = IntStream.rangeClosed(0, 7).mapToObj(e -> createTestBoard()).toList();
+		boardRepository.saveAll(boards);
+
+		// when
+		List<Board> dailyBoards = boardRepository.findDailyBest();
+
+		// then
+		Assertions.assertThat(dailyBoards).as("비어있는지 여부").isEmpty();
+	}
+
+	@Test
+	@DisplayName("주간 베스트 찾기 테스트")
+	void findWeeklyBestBoards() {
+		// given
+		List<Board> boards = LongStream.rangeClosed(0, 10).mapToObj(this::createTestBoardTime).toList();
+		boardRepository.saveAll(boards);
+
+		boards.forEach(board -> IntStream.range(0, 3).forEach(i -> boardLikeRepository.save(createTestBoardLike(board))));
+
+		// when
+		List<Board> weeklyBoards = boardRepository.findWeeklyBest();
+
+		// then
+		Assertions.assertThat(weeklyBoards).as("비어있는지 여부").isNotEmpty();
+		Assertions.assertThat(weeklyBoards.size()).as("5개인지").isEqualTo(5);
+		Assertions.assertThat(weeklyBoards.stream().findFirst().get().getTitle()).isEqualTo("테스트");
+	}
+
+	@Test
+	@DisplayName("주간 베스트 못 찾기 테스트 - 좋아요가 없을 시")
+	void notFindWeeklyBestBoards() {
+		// given
+		List<Board> boards = LongStream.rangeClosed(0, 10).mapToObj(this::createTestBoardTime).toList();
+		boardRepository.saveAll(boards);
+
+		// when
+		List<Board> weeklyBoards = boardRepository.findWeeklyBest();
+
+		// then
+		Assertions.assertThat(weeklyBoards).as("비어있는지 여부").isEmpty();
+	}
 
 	private Board createTestBoard() {
 		return Board.builder()
@@ -102,5 +172,20 @@ class BoardRepositoryTest {
 				.member(member)
 				.createdTime(LocalDateTime.now())
 				.build();
+	}
+
+	private Board createTestBoardTime(Long days) {
+		return Board.builder()
+				.title("테스트")
+				.content("테스트")
+				.views(0)
+				.member(member)
+				.createdTime(LocalDateTime.now().minusDays(days))
+				.build();
+	}
+
+
+	private BoardLike createTestBoardLike(Board board) {
+		return BoardLike.createBoardLikeEntity(board, member);
 	}
 }
