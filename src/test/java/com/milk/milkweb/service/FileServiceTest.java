4c9b11d1e862cf8b89450caea3a83daf91354e71
KwonYH <yhoung11@gmail.com>
2023-11-13 01:08:46 +0900

    test: FileService Test 수정 및 추가 - TempDir 어노테이션 추가로 AfterAll 메소드 삭제 - 업로드 & 다운로드 테스트 추가

--- src/test/java/com/milk/milkweb/service/FileServiceTest.java
+++ src/test/java/com/milk/milkweb/service/FileServiceTest.java
@@ -1,36 +1,35 @@
 package com.milk.milkweb.service;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.mock.web.MockMultipartFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 @ExtendWith(MockitoExtension.class)
 class FileServiceTest {
 
-	private static final String uploadLocation = "testUploadLocation";
-
-	@Mock
-	private MultipartFile multipartFile;
+	@TempDir
+	private Path tempDir;
 
 	@InjectMocks
 	private FileService fileService;
-
-	@AfterAll
-	static void afterAll() {
-		deleteFile(uploadLocation);
-	}
 
 	@Test
 	@DisplayName("폴더 생성 테스트")
@@ -39,34 +38,62 @@ class FileServiceTest {
 		String datePath = new SimpleDateFormat("yyyy-MM-dd").format(new Date()).replace("-", File.separator);
 
 		// when
-		String result = fileService.makeDir(uploadLocation);
+		String result = fileService.makeDir(tempDir.toString());
 
 		// then
-		assertThat(new File(uploadLocation, datePath).exists()).as("존재 여부").isTrue();
+		assertThat(new File(tempDir.toString(), datePath).exists()).as("존재 여부").isTrue();
 		assertThat(result).isEqualTo(datePath);
 	}
 
-	/**
-	 * 테스트용 파일 생성후 삭제하는 메소드
-	 * 파일이면 삭제 폴더면 재귀 DFS
-	 * @param path 파일경로
-	 */
-	private static void deleteFile(String path) {
-		File deleteFolder = new File(path);
+	@Test
+	@DisplayName("업로드 테스트")
+	void uploadFileTest() throws IOException {
+		// given
+		String originalFilename = "testFile.txt";
 
-		if (deleteFolder.exists()) {
-			File[] deleteFolderList = deleteFolder.listFiles();
+		byte[] fileContent = "Test file content".getBytes();
+		MockMultipartFile mockMultipartFile = new MockMultipartFile(
+				"file",
+				originalFilename,
+				"text/plain",
+				fileContent);
 
-			for (File file : deleteFolderList) {
+		// When
+		String savedFileName = fileService.uploadFile(tempDir.toString(), mockMultipartFile);
 
-				if (file.isFile())
-					file.delete();
-				else
-					deleteFile(file.getPath());
+		// Then
+		assertThat(new File(tempDir.toString(), savedFileName).exists()).isTrue();
+	}
 
-				file.delete();
-			}
-			deleteFolder.delete();
-		}
+	@Test
+	@DisplayName("다운로드 테스트")
+	void downloadFileTest() throws IOException {
+		// given
+		String datePath = new SimpleDateFormat("yyyy-MM-dd").format(new Date()).replace("-", File.separator);
+		String fileName = "testFile.txt";
+
+		Path testFilePath = Paths.get(tempDir.toString(), datePath, fileName);
+		Files.createDirectories(testFilePath.getParent());
+		Files.createFile(testFilePath);
+
+		// when
+		Resource resource = fileService.downloadFile(tempDir.toString(), datePath, fileName);
+
+		// then
+		assertThat(resource).as("존재 여부").isNotNull();
+		assertThat(resource.exists()).isTrue();
+		assertThat(resource).isInstanceOf(FileSystemResource.class);
+	}
+
+	@Test
+	@DisplayName("다운로드 실패 테스트")
+	void downloadFileFailTest() {
+		// given
+		String datePath = new SimpleDateFormat("yyyy-MM-dd").format(new Date()).replace("-", File.separator);
+		String fileName = "noExist.txt";
+
+		// when, then
+		assertThatThrownBy(() -> fileService.downloadFile(tempDir.toString(), datePath, fileName)).as("Exception 던져요?")
+				.isInstanceOf(RuntimeException.class);
 	}
 }
