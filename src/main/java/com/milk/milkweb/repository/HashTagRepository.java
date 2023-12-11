56d44ee1641d4af683b47c0ad73e02613501937a
KwonYH <yhoung11@gmail.com>
2023-12-12 01:13:25 +0900

    feat: HashTagRepository 누락 추가

---
+++ src/main/java/com/milk/milkweb/repository/HashTagRepository.java
@@ -0,0 +1,7 @@
+package com.milk.milkweb.repository;
+
+import com.milk.milkweb.entity.HashTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HashTagRepository extends JpaRepository<HashTag, Long> {
+}
